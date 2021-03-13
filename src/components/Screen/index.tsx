a84bb88e5ec7c3b088103d232cc1aa9fdc5aefb8
ElbertRibeiro <45343415+ElbertRibeiro@users.noreply.github.com>
2021-03-13 12:46:58 -0300

    update project

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -1,32 +1,79 @@
 import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native';
 
 import style from './style';
 
+export default function Screen() {
 
-export default function Screen () {
+    const [numberSort, setNumberSort] = useState(0);
+    const [varMin, setVarMin] = useState("0");
+    const [varMax, setVarMax] = useState("10");
 
-    
+    const [flagInputFocus, setFlagInputFocus] = useState("");
 
-    const [ numberSort, setNumberSort ] = useState(0);
+    const validarCampos = (min: any, max: any) => {
+        if (isNaN(min) || isNaN(max)) {
+            alert('Digite os valores');
+            return false;
+        }
+
+        if (min > max) {
+            alert('O valor mínimo deve ser menor que o valor máximo');
+            return false;
+        }
+
+        return true;
+    }
 
     const gerateNumber = () => {
-        const newNumber = Math.floor(Math.random() * (101 - 1 ) + 1);
+        const min = parseInt(varMin);
+        const max = parseInt(varMax);
+
+        if (!validarCampos(min, max)) {
+            return;
+        }
+
+        const newNumber = Math.floor(Math.random() * (max + 1 - min) + min);
         setNumberSort(newNumber);
     }
 
     return (
         <View style={style.screen}>
-            <Text style={style.title}>Toque no botão e veja quem é o vencedor</Text>
+            <Text style={style.title}>Escolha os valores Minimo e Máximo para realizar o sorteio</Text>
 
-            <View style={style.boxNumber}>
-                <Text style={style.number}>
-                    { numberSort }
-                </Text>
+            <View style={style.lineInput}>
+                <Text>Valor Minimo: </Text>
+                <TextInput
+                    value={varMin.toString()}
+                    onChangeText={value => setVarMin(value)}
+                    textAlign="center"
+                    keyboardType="number-pad"
+                    maxLength={5}
+                    autoFocus={true}
+                    style={flagInputFocus === "txt_min" ? style.inputFocus : style.inputNormal}
+                    onFocus={() => setFlagInputFocus("txt_min")}
+                    onBlur={() => setFlagInputFocus("")} />
+            </View>
+
+            <View style={style.lineInput}>
+                <Text>Valor Máximo: </Text>
+                <TextInput
+                    textAlign="center"
+                    keyboardType="number-pad"
+                    maxLength={5}
+                    style={flagInputFocus === "txt_max" ? style.inputFocus : style.inputNormal}
+                    onFocus={() => setFlagInputFocus("txt_max")}
+                    onBlur={() => setFlagInputFocus("")} />
             </View>
 
             <View style={style.boxButton}>
-                <Button title="Sortear" onPress={gerateNumber} color="#1F4F66"/>
+                <Button title="Sortear" onPress={gerateNumber} color="#1F4F66" />
+            </View>
+
+            <View style={style.boxNumber}>
+                <Text style={style.number}>
+                    {numberSort}
+                </Text>
             </View>
         </View>
     );
--- src/components/Screen/style.tsx
+++ src/components/Screen/style.tsx
@@ -21,14 +21,37 @@ const style = StyleSheet.create({
         borderRadius: 75,
         justifyContent: "center",
         alignItems: "center",
+        marginTop: 50,
         marginBottom: 50
     },
     boxButton: {
-        width: 200
+        width: 200,
+        marginTop: 20
     },
     number: {
-        fontSize: 80,
+        fontSize: 50,
         color: '#fff'
+    },
+    lineInput: {
+        flexDirection: 'row',
+        alignItems: "center",
+        marginBottom: 10
+    },
+    inputFocus: {
+        borderWidth: 1,
+        borderColor: '#1f4f66',
+        borderRadius: 3,
+        width: 100,
+        paddingHorizontal: 5,
+        marginLeft: 10
+    },  
+    inputNormal: {
+        borderWidth: 1,
+        borderColor: '#d5d5d5',
+        borderRadius: 3,
+        width: 100,
+        paddingHorizontal: 5,
+        marginLeft: 10
     }
 });
 
